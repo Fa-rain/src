32fc043288a629141e78e3002a1dc0704e6e7289
Fa-rain <farainableristian0@gmail.com>
2025-05-14 11:28:24 +0700

    menambahkan 3 file java

---
+++ Flora.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ulasan;
+
+/**
+ *
+ * @author DELL
+ */
+public class Flora {
+    String nama = "kelengkeng", buah, batang;
+    int usia;
+}
